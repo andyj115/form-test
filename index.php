5f281d3f0d1ef03e82608c7bd67189f0660b9fc3
Andy <john.a.johnson@1eye.io>
2017-01-31 20:35:30 +0100

    developing

--- index.php
+++ index.php
@@ -12,23 +12,23 @@ function GetCurVal( $sKey, $sVal = '' )
     return $sVal;
 }
 ?><!DOCTYPE html>
-<html lang="en">
+<html lang='en'>
     <head>
-        <meta charset="utf-8"> 
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset='utf-8'> 
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
     
 	<title>1eye.io - Form test</title>
 		
-        <meta name="robots" content="noindex, nofollow">
-        <link rel="icon" href="favicon.ico">
+        <meta name='robots' content='noindex, nofollow'>
+        <link rel='icon' href='favicon.ico'>
 
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+        <link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
+        <link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css'>
         
         <!--[if lt IE 9]>
-            <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-            <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+            <script src='//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js'></script>
+            <script src='//oss.maxcdn.com/respond/1.4.2/respond.min.js'></script>
         <![endif]-->
         
         <link href='//fonts.googleapis.com/css?family=Work+Sans:400,300,600' rel='stylesheet' type='text/css'>
@@ -47,48 +47,48 @@ function GetCurVal( $sKey, $sVal = '' )
         </style>
     </head>
     <body>
-        <section class="main-container">
-            <section class="content-body">
-                <div class="row">
-                    <div class="col-sm-8">
+        <section class='main-container'>
+            <section class='content-body'>
+                <div class='row'>
+                    <div class='col-sm-8'>
                         <h2>Form store test (ajax/restful)</h2>
-                        <form class="form-horizontal">
-                            <div class="form-group">
-                                <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
-                                <div class="col-sm-10">
-                                <input type="email" class="form-control" id="inputEmail3" placeholder="Email" value="<?= GetCurVal('inputEmail3') ?>">
+                        <form class='form-horizontal'>
+                            <div class='form-group'>
+                                <label for='inputEmail3' class='col-sm-2 control-label'>Email</label>
+                                <div class='col-sm-10'>
+                                <input type='email' class='form-control' id='inputEmail3' placeholder='Email' value='<?= GetCurVal('inputEmail3') ?>'>
                                 </div>
                             </div>
-                            <div class="form-group">
-                                <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
-                                <div class="col-sm-10">
-                                <input type="text" class="form-control" id="inputPassword3" placeholder="Password" value="<?= GetCurVal('inputPassword3') ?>">
+                            <div class='form-group'>
+                                <label for='inputPassword3' class='col-sm-2 control-label'>Password</label>
+                                <div class='col-sm-10'>
+                                <input type='text' class='form-control' id='inputPassword3' placeholder='Password' value='<?= GetCurVal('inputPassword3') ?>'>
                                 </div>
                             </div>
-                            <div class="form-group">
-                                <label for="name" class="col-sm-2 control-label">Name</label>
-                                <div class="col-sm-10">
-                                <input type="text" class="form-control" id="name" placeholder="Full Name" value="<?= GetCurVal('name') ?>">
+                            <div class='form-group'>
+                                <label for='name' class='col-sm-2 control-label'>Name</label>
+                                <div class='col-sm-10'>
+                                <input type='text' class='form-control' id='name' placeholder='Full Name' value='<?= GetCurVal('name') ?>'>
                                 </div>
                             </div>
-                            <div class="form-group">
-                                <label for="address" class="col-sm-2 control-label">Address</label>
-                                <div class="col-sm-10">
-                                <input type="text" class="form-control" id="address" placeholder="Address" value="<?= GetCurVal('address') ?>">
+                            <div class='form-group'>
+                                <label for='address' class='col-sm-2 control-label'>Address</label>
+                                <div class='col-sm-10'>
+                                <input type='text' class='form-control' id='address' placeholder='Address' value='<?= GetCurVal('address') ?>'>
                                 </div>
                             </div>
-                            <div class="form-group">
-                                <div class="col-sm-offset-2 col-sm-10">
-                                <div class="checkbox">
+                            <div class='form-group'>
+                                <div class='col-sm-offset-2 col-sm-10'>
+                                <div class='checkbox'>
                                     <label>
-                                    <input type="checkbox" id="remember"> Remember me
+                                    <input type='checkbox' id='remember' value='<?= GetCurVal('remember') ?>'> Remember me
                                     </label>
                                 </div>
                                 </div>
                             </div>
-                            <div class="form-group">
-                                <div class="col-sm-offset-2 col-sm-10">
-                                    <button type="button" class="btn btn-default" id="ok">Sign in</button>
+                            <div class='form-group'>
+                                <div class='col-sm-offset-2 col-sm-10'>
+                                    <button type='button' class='btn btn-default' id='ok'>Sign in</button>
                                     <i>(check events in javascript console)</i>
                                 </div>
                             </div>
@@ -99,13 +99,13 @@ function GetCurVal( $sKey, $sVal = '' )
             </section>
         </section>
 
-        <script src="//code.jquery.com/jquery-3.1.1.min.js"></script>
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script src='//code.jquery.com/jquery-3.1.1.min.js'></script>
+        <script src='//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js' integrity='sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa' crossorigin='anonymous'></script>
         <script>
             $(document).ready(function() {
-                
-                $('input').each(function() {
-                    $(this).on("change", function() {
+
+                $('input:text').each(function() {
+                    $(this).on('change', function() {
                         console.log( 'Store ID ' + $(this).attr('id') + ': ' + $(this).val() );            
                         $.post({
                             url: 'api/',
@@ -116,8 +116,16 @@ function GetCurVal( $sKey, $sVal = '' )
                         });
                     });
                 });
-                
-                $('#ok').on("click", function() {
+
+                $('input:checkbox').each(function() {
+                    $(this).change(function () {
+                        var name = $(this).attr('id');
+                        var check = $(this).attr('checked');
+                        console.log( 'Change ID ' + name + ' to ' + check );
+                    });
+                });
+
+                $('#ok').on('click', function() {
                     $('input').each(function() {
                         console.log( ' - ID ' + $(this).attr('id') + ': ' + $(this).val() );            
                         $.post({
@@ -128,7 +136,7 @@ function GetCurVal( $sKey, $sVal = '' )
                             }
                         });
                     });
-                    console.warn( 'Ok, go on!!' );            
+                    console.log( 'Ok, go on!!' );            
                     $.post({
                         url: 'api/',
                         data: {
