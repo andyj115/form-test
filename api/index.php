7a30e083aab177410fe489cc35109c40e46589da
Andy <12398372+andyj115@users.noreply.github.com>
2019-06-07 12:26:59 +0200

    updated

--- api/index.php
+++ api/index.php
@@ -7,10 +7,11 @@ if( isset( $_POST )  &&  isset( $_POST['id'] )  &&  isset( $_POST['val'] ) )
     $_SESSION[ 'fv' ][ $_POST['id'] ] = $_POST['val'];
 
     header( "Content-type: application/json" );
-    echo( json_encode( array(
+    echo( json_encode( [
         'stat' 	=> 'ok',
         'id' 	=> $_POST[ 'id' ],
-        'val' 	=> $_POST[ 'val' ] ) ) );
+        'val' 	=> $_POST[ 'val' ],
+        'array' => $_SESSION[ 'fv' ] ] ) );
 
     return;
 }
